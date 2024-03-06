659e1a22ea4cb4d551b5b7d38b866949ba288ffd
Letícia Faria <le2006faria@gmail.com>
2024-03-06 16:38:13 -0300

    adicionando arquivos

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Animais from './pages/Animais/Animais';
 import Home from './pages/Home/Home';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Home />
+      <Animais />
     </>
   )
 }
