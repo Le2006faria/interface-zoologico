85fe3bb39023ce09203744775bbcbf30a62779cd
Letícia Faria <le2006faria@gmail.com>
2024-03-08 11:23:43 -0300

    adicionando as páginas: fotos, depoimentos e login

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css'
 import Animais from './pages/Animais/Animais';
+import Depoimentos from './pages/Depoimentos/Depoimentos';
+import Fotos from './pages/Fotos/Fotos';
 import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
@@ -10,7 +13,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route exact path='/' Component={Home} />
-          <Route path='/animais' Component={Animais} />
+          <Route exact path='/animais' Component={Animais} />
+          <Route exact path='/fotos' Component={Fotos} />
+          <Route exact path='/depoimentos' Component={Depoimentos} />
+          <Route exact path='/login' Component={Login} />
         </Routes>
       </BrowserRouter>
     </>
