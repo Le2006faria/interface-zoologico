baea883363e735444d3b8ed127c14024fbb3608d
Letícia Faria <le2006faria@gmail.com>
2024-05-03 16:49:45 -0300

    listando animais

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,13 @@
 import './App.css'
-import Animais from './pages/Animais/Animais';
-import CadastroDeAve from './pages/CadastroDeAve/CadatroDeAve';
-import Depoimentos from './pages/Depoimentos/Depoimentos';
-import Fotos from './pages/Fotos/Fotos';
-import Home from './pages/Home/Home';
-import Login from './pages/Login/Login';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import AppRoutes from './routes'
 
 function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route exact path='/' Component={Home} />
-          <Route exact path='/animais' Component={Animais} />
-          <Route exact path='/fotos' Component={Fotos} />
-          <Route exact path='/depoimentos' Component={Depoimentos} />
-          <Route exact path='/login' Component={Login} />
-          <Route exact path='/cadastroDeAve' Component={CadastroDeAve} />
-        </Routes>
-      </BrowserRouter>
+    <AppRoutes />
     </>
   )
 }
-////bom dia
+
 export default App
--- src/pages/Animais/Animais.jsx
+++ src/pages/Animais/Animais.jsx
@@ -1,14 +1,57 @@
 import Navegacao from '../../components/Navegacao/Navegacao';
 import CardAnimal from '../../components/CardAnimal/CardAnimal';
 import './Animais.css'
+import React, { useEffect, useState } from 'react';
 
-function Animais(){
-    return(
+function Animais() {
+
+    // RECUPERANDO LISTA DE ANIMAIS DO SERVIDOR
+    const [animais, setAnimais] = useState(null);
+
+    // Recupera a lista de todos os animais do servidor
+    useEffect(() => {
+		    // função para fazer o fetch (busca) das informações na API
+        const fetchData = async () => {
+		        // tenta fazer aa busca
+            try {
+		            // Faz a busca e armazena o resultado em response
+                const response = await fetch('http://localhost:3000/listar-aves');
+                // verifica se o estado da response (ok) é falso
+                if (!response.ok) {
+		                // caso seja falso, houve algum erro na requisição, e o erro é lançado
+                    throw new Error('Erro ao buscar servidor');
+                }
+                // caso a busca seja bem sucedida, o resultado é convertido em um JSON e armazenado
+                // na variável listaAnimais
+                const listaAnimais = await response.json();
+                // atribui o valor de listaAnimais a variável animais
+                // usando o setAnimais
+                setAnimais(listaAnimais);
+            // caso haja algum erro na busca, é disparado o catch
+            } catch (error) {
+		            // escreve o erro no console
+                console.error('Erro: ', error);
+            }
+        }
+
+				 // executa a função fetchData
+        fetchData();
+    }, []);
+    return (
         <>
-        <Navegacao />
-        <CardAnimal />
+            <Navegacao />
+            <div className='ctn-animais'>
+                {animais ? (
+                    // Renderize o seu componente para cada item da lista
+                    animais.map(animal => (
+                        <CardAnimal key={animal.idAnimal} animal={animal} />
+                    ))
+                ) : (
+                    <p>Carregando... Verifique se o servidor está funcionando</p>
+                )}
+            </div>
         </>
     );
 }
 
-export default Animais
+export default Animais;
